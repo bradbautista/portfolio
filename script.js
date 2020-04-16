ca1f9fc0e5f4b092b0f7384c068b4b3d9b1b9fa2
unknown <bradbautista@gmail.com>
2020-04-15 20:15:39 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -6,22 +6,26 @@ function hideNav() {
     let lastScroll = 0;
     
     window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset;
-    if (currentScroll == 0) {
-        body.classList.remove(scrollUp);
+        
+        const currentScroll = window.pageYOffset;
+
+        if (currentScroll == 0) {
+            body.classList.remove(scrollUp);
         return;
-    }
+        }
     
-    if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-        // down
-        body.classList.remove(scrollUp);
-        body.classList.add(scrollDown);
-    } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-        // up
-        body.classList.remove(scrollDown);
-        body.classList.add(scrollUp);
-    }
-    lastScroll = currentScroll;
+        if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+            // down
+            body.classList.remove(scrollUp);
+            body.classList.add(scrollDown);
+        } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+            // up
+            body.classList.remove(scrollDown);
+            body.classList.add(scrollUp);
+        }
+
+        lastScroll = currentScroll;
+
     });
 
 
